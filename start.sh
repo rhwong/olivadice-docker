feccb32342420180e49c09db3e8663260f8bfa31
贰狐RH <58497504+rhwong@users.noreply.github.com>
2021-12-11 00:51:45 +0800

    commit

--- start.sh
+++ start.sh
@@ -1,2 +1,6 @@
+#!/bin/bash
+
+sed -i "s/123456/$BOTQQ/g" config.yml
+sed -i "s/123456/$BOTQQ/g" /workspace/OlivOS/conf/account.json
 cd /workspace/OlivOS && nohup python3 main.py >> my.log 2>&1 &
 cd /workspace && ./go-cqhttp faststart
